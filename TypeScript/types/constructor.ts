5474d524130c6ca8db2d638d311e3b77e3ea203c
ryo-ohki-code <ryo-ohki-node@protonmail.com>
2025-10-19 23:25:18 +0200

    Create constructor.ts

---
+++ TypeScript/types/constructor.ts
@@ -0,0 +1,12 @@
+export interface SideshiftAPIOptions {
+    secret: string;
+    id: string;
+    commissionRate?: string;
+    verbose?: boolean;
+    retries?: {
+        maxRetries?: number;
+        retryDelay?: number;
+        retryBackoff?: number;
+        retryCappedDelay?: number;
+    }; 
+}
